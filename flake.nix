75ed1c68eb73e68361f4c9a6b5bcf6b5802f2f4e
Jackson Breit <84806918+NinjacksonXV@users.noreply.github.com>
2025-03-01 12:06:17 -0800

    Add Pico flake, submodule

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Generic Raspberry Pi Pico development environment";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cmake
+          ninja
+          gcc-arm-embedded
+          python3
+          git
+          picotool
+        ];
+
+        shellHook = ''
+
+        '';
+      };
+    };
+}
